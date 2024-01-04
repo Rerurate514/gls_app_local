73ec4290e66f652fb6e7e8afac9683124c736ba7
Rerurate514 <rerurateyuto@gmail.com>
2024-01-05 03:30:13 +0900

    ADクラスの分離

---
+++ lib/Ad/adInfo.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+enum AdTableColumn{
+  AD_CREATER,
+  AD_IMAGE_URL,
+  AD_TITLE,
+  AD_DETAIL,
+  AD_TARGET_MONEY_AMOUNT,
+  AD_TOTAL_MONEY_AMOUNT,
+  AD_DEADLINE,
+  AD_TARGET_AIDOL,
+  AD_TARGET_PLATFORM,
+  AD_CATEGORY,
+  AD_HASHTAG,
+  AD_AIDER_NUMBERS,
+  AD_CREATED,
+}
+
+class AdInfo{
+  late final String _adId;
+
+  late final String _creater;
+  late final String _imageUrl;
+  late final String _title;
+  late final String _detail;
+  late final int _targetMoneyAmount;
+  int _totalMoneyAmount = 0;
+  late final Timestamp _deadline;
+  late final String _targetIdol;
+  late final String _targetPlatform;
+  late final String _category;
+  late final String _hashtag;
+  int _aiderNumbers = 0;
+  late final Timestamp _created;
+
+  late Map<String, dynamic> _dbProcessedMap;
+
+  String get adId => _adId;
+  Map<String, dynamic> get dbProcessedMap => _dbProcessedMap; 
+
+  AdInfo({
+    required String creater,
+    required String imageUrl,
+    required String title,
+    required String detail,
+    required int targetMoneyAmount,
+    required Timestamp deadline,
+    required String targetIdol,
+    required String targetPlatform,
+    required List<String> category,
+    required List<String> hashtag,
+  }){
+    _creater = creater;
+    _imageUrl = imageUrl;
+    _title = title;
+    _detail = detail;
+    _targetMoneyAmount = targetMoneyAmount;
+    _deadline = deadline;
+    _targetIdol = targetIdol;
+    _targetPlatform = targetPlatform;
+    _category = category.join(",");
+    _hashtag = hashtag.join(",");
+
+    _created = Timestamp.now();
+
+    _adId = _creater + ":" + _title + ":" + _created.toString();
+
+    _dbProcessedMap = _createDbProcessedMap();
+  } 
+
+  Map<String, dynamic> _createDbProcessedMap(){
+    return {
+      AdTableColumn.AD_CREATER.name:_creater,
+      AdTableColumn.AD_IMAGE_URL.name:_imageUrl,
+      AdTableColumn.AD_TITLE.name:_title,
+      AdTableColumn.AD_DETAIL.name:_detail,
+      AdTableColumn.AD_TARGET_MONEY_AMOUNT.name:_targetMoneyAmount,
+      AdTableColumn.AD_TOTAL_MONEY_AMOUNT.name:_totalMoneyAmount,
+      AdTableColumn.AD_DEADLINE.name:_deadline,
+      AdTableColumn.AD_TARGET_AIDOL.name:_targetIdol,
+      AdTableColumn.AD_TARGET_PLATFORM.name:_targetPlatform,
+      AdTableColumn.AD_CATEGORY.name:_category,
+      AdTableColumn.AD_HASHTAG.name:_hashtag,
+      AdTableColumn.AD_AIDER_NUMBERS.name:_aiderNumbers,
+      AdTableColumn.AD_CREATED.name:_created,
+    };
+  }
+}
